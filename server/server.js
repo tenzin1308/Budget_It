21bab8054c6438ef493a037fce6b118dbd1c30e5
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-04 17:14:01 -0500

    cloud db connected

--- server/server.js
+++ server/server.js
@@ -17,10 +17,14 @@ const port = 8080
 mongoose.connect(
     process.env.BudgetIt_DB_URL || 'mongodb://localhost/budgetIt',
     {
-        maxPoolSize:50,
-        wtimeoutMS:2500,
-        useNewUrlParser:true,
-        useUnifiedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000,
+        autoIndex: false, // Don't build indexes
+        maxPoolSize: 10, // Maintain up to 10 socket connections
+        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        family: 4 // Use IPv4, skip trying IPv6
     }
 )
 
