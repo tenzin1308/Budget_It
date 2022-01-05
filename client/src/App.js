15e4d265f42518d5a7b99f418e2ebb0ae68d5ed6
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-04 19:42:44 -0500

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <NavBar />
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/products" element={<Products />} />
+          </Routes>
+
+        </div>
+      </Router>
     </div>
+    
   );
 }
 
---
+++ client/src/pages/Products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Products() {
+    return (
+        <div>
+            <h2>Product page</h2>
+        </div>
+    )
+}
