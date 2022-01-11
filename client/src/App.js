ded0fd080af8891569a580ce69e19333765067b8
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-11 12:34:32 -0500

    fix: about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
 import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
